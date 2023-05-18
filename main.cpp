5747afdbcbb83a949dbc911430c05a04be0d0484
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-18 05:33:54 +0000

    looking for a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
-#include <iostream>
+#include <string>
 
-int main() {
-  std::cout << "Hello World!\n";
+class Node {
+  public:
+    std::string val;
+    Node* next;
+
+    Node(std::string initialVal) {
+      val = initialVal;
+      next = nullptr;
+    }
+};
+
+
+
+bool linkedListFind(Node* head, std::string target) 
+{
+  Node * current = head;
+  while (current != nullptr)
+  {
+    if(current->val==target) return true;
+    current=current->next;
+  }
+  return false;
+}
+
+int main()
+{
+  Node a("A");
+  Node b("B");
+  a.next=&b;
+  linkedListFind(a, "A");
+
+  return 0;
 }
